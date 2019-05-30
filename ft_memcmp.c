c88f5b0dda8f07b94067cd2d4f4609b14f9c5597
Mthokozisi NZOLO <mnzolo@c5r2s6.wethinkcode.co.za>
2019-05-30 17:43:49 +0200

    ft_atoi.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: mnzolo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 11:29:12 by mnzolo            #+#    #+#             */
-/*   Updated: 2019/05/30 17:33:35 by mnzolo           ###   ########.fr       */
+/*   Updated: 2019/05/30 17:41:49 by mnzolo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,11 @@ int	ft_memcmp(void *s1, void *s2, size_t n)
 	i = 0;
 	a = (unsigned char*)s1;
 	b = (unsigned char*)s2;
-	while ( i < n - 1 && a[i] == b[i])
+	if (n == 0)
+	{
+		return (0);
+	}
+	while (i < n - 1 && a[i] == b[i])
 	{
 		i++;
 	}
