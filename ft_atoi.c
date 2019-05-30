6dd21584afd8963651c35cfc20ccb6c72db0f3fe
Mthokozisi NZOLO <mnzolo@c5r2s6.wethinkcode.co.za>
2019-05-30 08:28:34 +0200

    Makefile

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,22 +6,21 @@
 /*   By: mnzolo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 13:56:26 by mnzolo            #+#    #+#             */
-/*   Updated: 2019/05/27 09:49:53 by mnzolo           ###   ########.fr       */
+/*   Updated: 2019/05/28 12:54:38 by mnzolo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdio.h>
+#include "libft.h"
 
-int		ft_isdigit(int i)
+int	ft_isdigit(int i)
 {
 	if (i >= '0' && i <= '9')
 		return (1);
-	else 
+	else
 		return (0);
 }
 
-int		ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
 	int i;
 	int neg;
@@ -29,33 +28,19 @@ int		ft_atoi(const char *str)
 
 	i = 0;
 	neg = 1;
-		while(str[i] == ' ' || str[i] == '\t' || str[i] == '\n' )
-		{
-			i++;
-		}
-		
-		if( str[i] == '-')
-			neg = neg * -1;
-		
-		if ( str[i] == '-' || str[i] == '+')
-			i++;
-		res = 0;
-		while(ft_isdigit(str[i]))
-		{
-			res = res * 10 + (str[i] - '0');
-			i++;
-		}
-	return(res * neg);
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+	{
+		i++;
+	}
+	if (str[i] == '-')
+		neg = neg * -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	res = 0;
+	while (ft_isdigit(str[i]))
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * neg);
 }
-
-int main(void)
-{
-    int i;
-    char *s;
- 
-    s = " -9885";
-    i = ft_atoi(s);     /* i = -9885 */
- 
-    printf("i = %d\n",i);
-}
-
