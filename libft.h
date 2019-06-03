ef280b1b3aedf09ef4734f6cd788d76dfa0695e7
Mthokozisi NZOLO <mnzolo@c5r2s6.wethinkcode.co.za>
2019-06-03 18:15:58 +0200

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mnzolo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 12:11:23 by mnzolo            #+#    #+#             */
-/*   Updated: 2019/06/01 14:39:27 by mnzolo           ###   ########.fr       */
+/*   Updated: 2019/06/03 13:24:49 by mnzolo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ void			*ft_memchr(void *s, int c, size_t n);
 int				ft_memcmp(void *s1, void *s2, size_t n);
 void			*ft_memcpy(void *dst, void *src, size_t n);
 void			*ft_memset(void *b, int c, size_t len);
+void			*ft_memmove(void *dst, const void *src, size_t len);
 void			ft_putchar(char c);
 void			ft_putchar_fd(char c, int fd);
 char			*ft_strcat(char *s1, char *s2);
